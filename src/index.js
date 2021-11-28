5207262524890b1c9986f339264f13c2e8f1be75
Alex Barker <42222405+six-edge@users.noreply.github.com>
2021-11-28 22:19:49 +0000

    Updated header and description

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import "./styles.css";
 
 document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
+<h1>JS Sandbox</h1>
 <div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+  <p>A place to play with code 😀</p>
 </div>
 `;
